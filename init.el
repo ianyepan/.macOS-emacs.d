4aee466d5bfbcceb69338484078f7815a2004c66
Ian Pan <the_happy_hacker@YCtekiMacBook-Pro.local>
2019-12-31 19:01:16 +0800

    Add evil-collection for more vi-style support

--- init.el
+++ init.el
@@ -301,8 +301,9 @@
 (use-package evil
   :diminish undo-tree-mode
   :init
-  (setq evil-want-C-u-scroll t)
-  (setq evil-shift-width ian/indent-width)
+  (setq evil-want-C-u-scroll t
+        evil-want-keybinding nil
+        evil-shift-width ian/indent-width)
   :hook (after-init . evil-mode)
   :preface
   (defun ian/save-and-kill-this-buffer ()
@@ -313,13 +314,17 @@
   (with-eval-after-load 'evil-maps ; avoid conflict with company tooltip selection
     (define-key evil-insert-state-map (kbd "C-n") nil)
     (define-key evil-insert-state-map (kbd "C-p") nil))
-  (evil-set-initial-state 'term-mode 'emacs)
   (evil-ex-define-cmd "q" #'kill-this-buffer)
-  (evil-ex-define-cmd "wq" #'ian/save-and-kill-this-buffer)
-  (use-package evil-commentary
-    :after evil
-    :diminish
-    :config (evil-commentary-mode +1)))
+  (evil-ex-define-cmd "wq" #'ian/save-and-kill-this-buffer))
+
+(use-package evil-collection
+  :after evil
+  :config (evil-collection-init))
+
+(use-package evil-commentary
+  :after evil
+  :diminish
+  :config (evil-commentary-mode +1))
 
 (use-package evil-magit)
 
